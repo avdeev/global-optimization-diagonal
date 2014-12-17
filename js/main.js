abc85ec5d96822a202046b513162f9e338350354
Alexey Avdeev <aad.jerry@gmail.com>
2014-12-17 13:11:53 +0300

    Basic functional

---
+++ js/main.js
@@ -0,0 +1,187 @@
+var Point = function(x, y) {
+  this.x = x;
+  this.y = y;
+
+  this.render = function() {
+    canvasContext.beginPath();
+
+    canvasContext.arc(
+      (this.x - xShift) * xSize,
+      (this.y - yShift) * ySize,
+      5, 0, Math.PI * 2
+    );
+
+    canvasContext.closePath();
+
+    canvasContext.fillStyle = 'red';
+    canvasContext.fill();
+
+    return this;
+  }
+}
+
+var Interval = function(a, b) {
+  this.a = a;
+  this.b = b;
+
+  this.x = Math.abs(this.b.x - this.a.x);
+  this.y = Math.abs(this.b.y - this.a.y);
+
+  this.diagonal = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+
+  this.R = (f.eval(this.a) + f.eval(this.b)) / 2 - fLipschitz * this.diagonal / 2;
+
+  this.clear = function() {
+    canvasContext.clearRect(
+      (this.a.x - xShift) * xSize,
+      (this.a.y - yShift) * ySize,
+      this.x * xSize,
+      this.y * ySize
+    );
+
+    return this;
+  }
+
+  this.render = function() {
+    canvasContext.beginPath();
+
+    canvasContext.rect(
+      (this.a.x - xShift) * xSize,
+      (this.a.y - yShift) * ySize,
+      this.x * xSize,
+      this.y * ySize
+    );
+
+    canvasContext.closePath();
+
+    opacity = 0.2;
+    canvasContext.fillStyle = "rgba(50, 100, 50, " + opacity + ")";
+    canvasContext.fill();
+
+    canvasContext.lineWidth = 1;
+    canvasContext.strokeStyle = 'black';
+    canvasContext.stroke();
+
+    return this;
+  }
+
+  this.getS = function() {
+    var temp = (f.eval(this.b) - f.eval(this.a)) / (2 * fLipschitz);
+    var x = ((this.a.x + this.b.x) / 2) - temp;
+    var y = ((this.a.y + this.b.y) / 2) - temp;
+    return new Point(x, y);
+  }
+}
+
+var IntervalCollection = function(interval) {
+  this.c = [interval];
+
+  this.base = interval;
+
+  this.clear = function() {
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  this.render = function() {
+    this.clear();
+    for (var i = 0; i < this.c.length; i++) {
+      this.c[i].render();
+    }
+  }
+
+  this.getMinRArg = function() {
+    var min = this.c[0].R;
+    var t = 0;
+    for (var i = 1; i < this.c.length; i++) {
+      if (this.c[i].R < min) {
+        min = this.c[i].R;
+        t = i;
+      }
+    }
+    return t;
+  }
+}
+
+var calculate = function() {
+  window.canvas = document.getElementById("visualization");
+  window.canvasContext = canvas.getContext("2d");
+
+  window.D = new IntervalCollection(new Interval(
+    new Point(lowBound, lowBound),
+    new Point(highBound, highBound)
+  ));
+
+  window.xSize = canvas.width / D.base.x;
+  window.ySize = canvas.height / D.base.y;
+
+  window.xShift = D.base.a.x;
+  window.yShift = D.base.a.y;
+
+  D.clear();
+
+  var a, b, active, S;
+  do {
+    active = D.getMinRArg();
+
+    // точка разбиения
+    S = D.c[active].getS();
+    S.render();
+
+    // разбиение по длинной стороне
+    a = D.c[active].a;
+    b = D.c[active].b;
+
+    if (D.c[active].x > D.c[active].y) {
+      D.c[active] = new Interval(
+        a,
+        new Point(S.x, b.y)
+      );
+
+      D.c.push(new Interval(
+        new Point(S.x, a.y),
+        b
+      ));
+
+    } else {
+      D.c[active] = new Interval(
+        new Point(a.x, S.y),
+        b
+      );
+
+      D.c.push(new Interval(
+        a,
+        new Point(b.x, S.y)
+      ));
+    }
+
+    D.c[D.c.length - 2].render();
+    D.c[D.c.length - 1].render();
+
+  } while (D.c[active].diagonal > (eps * D.base.diagonal) && D.c.length < 10000);
+}
+
+
+$(function() {
+  $('#run').on('click', function(e) {
+    e.preventDefault();
+
+    var params = $(e.target).closest('form').serializeArray();
+    $(params).each(function() {
+      params[this.name] = this.value;
+    });
+
+    window.f = math.compile(params['f']);;
+    window.fLipschitz = parseFloat(params['fLipschitz']);
+    window.lowBound = parseFloat(params['lowBound']);
+    window.highBound = parseFloat(params['highBound']);
+    window.eps = parseFloat(params['eps']);
+
+    calculate();
+
+    $('#d-length').text(D.c.length);
+    // $('#active-x').text(D.c.length);
+    // $('#active-y').text(D.c.length);
+    // $('#active-f').text(D.c.length);
+    
+  });
+});
